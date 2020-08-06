e80e6ad0ede1c917f99354eb9c4c40f6ab07c2aa
Michael Bender <mbender@bentech.net>
2020-08-06 18:58:19 -0500

    Update iis-bootstrap.ps1

--- terraform/azlb/iis-bootstrap.ps1
+++ terraform/azlb/iis-bootstrap.ps1
@@ -2,7 +2,7 @@ Add-WindowsFeature Web-Server
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
 New-Item -ItemType directory -Path "C:\inetpub\wwwroot\images"
 New-Item -ItemType directory -Path "C:\inetpub\wwwroot\video"
-$imagevalue = "Images: " + $($env:computername)
+$imagevalue = "Image Server: " + $($env:computername)
 Add-Content -Path "C:\inetpub\wwwroot\images\test.htm" -Value $imagevalue
-$videovalue = "Video: " + $($env:computername)
+$videovalue = "Video Server: " + $($env:computername)
 Add-Content -Path "C:\inetpub\wwwroot\video\test.htm" -Value $videovalue
