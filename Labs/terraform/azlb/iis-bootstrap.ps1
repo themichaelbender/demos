770bdfa169eb60475b817d9828e417084634f189
Michael Bender <mbender@bentech.net>
2020-08-10 10:21:09 -0500

    Update iis-bootstrap.ps1
    Removed video/image references

--- Labs/terraform/azlb/iis-bootstrap.ps1
+++ Labs/terraform/azlb/iis-bootstrap.ps1
@@ -1,8 +1,4 @@
 Add-WindowsFeature Web-Server
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
-New-Item -ItemType directory -Path "C:\inetpub\wwwroot\images"
-New-Item -ItemType directory -Path "C:\inetpub\wwwroot\video"
-$imagevalue = "Image Server: " + $($env:computername)
-Add-Content -Path "C:\inetpub\wwwroot\images\test.htm" -Value $imagevalue
-$videovalue = "Video Server: " + $($env:computername)
-Add-Content -Path "C:\inetpub\wwwroot\video\test.htm" -Value $videovalue
+$webservervalue = "Web Server: " + $($env:computername)
+Add-Content -Path "C:\inetpub\wwwroot\default.htm" -Value $webservervalue
