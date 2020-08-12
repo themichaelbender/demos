ede1c2b7057ef602ceef983ee49d15c11d2e83a7
themichaelbender <mbender@bentech.net>
2020-08-12 08:33:47 -0500

    updated more scripts

--- Labs/terraform/azlb/bootstrap-iisimages.ps1
+++ Labs/terraform/azlb/bootstrap-iisimages.ps1
@@ -1,5 +1,5 @@
 Add-WindowsFeature Web-Server
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
 New-Item -ItemType directory -Path "C:\inetpub\wwwroot\images"
-New-Item -ItemType directory -Path "C:\inetpub\wwwroot\video"imagevalue = "Image Server: " + $($env:computername)
+$imagevalue = "Image Server: " + $($env:computername)
 Add-Content -Path "C:\inetpub\wwwroot\images\test.htm" -Value $imagevalue
