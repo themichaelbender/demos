28e4391353009327d904ea4ce531162d311daf0e
themichaelbender <mbender@bentech.net>
2020-08-18 05:17:31 -0500

    updates

--- Labs/terraform/azlb/iis-bootstrap.ps1
+++ Labs/terraform/azlb/iis-bootstrap.ps1
@@ -1,4 +1,3 @@
 Add-WindowsFeature Web-Server
-Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
 $webservervalue = "Web Server: " + $($env:computername)
 Add-Content -Path "C:\inetpub\wwwroot\default.htm" -Value $webservervalue
