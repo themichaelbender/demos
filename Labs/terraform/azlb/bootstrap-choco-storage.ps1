651d585723dbef770d618bf0e5c5efe5f1015b00
Michael Bender <mbender@bentech.net>
2020-09-11 15:51:41 -0500

    Update bootstrap-choco-storage.ps1

--- Labs/terraform/azlb/bootstrap-choco-storage.ps1
+++ Labs/terraform/azlb/bootstrap-choco-storage.ps1
@@ -4,3 +4,5 @@ $pdfvalue = "Just a PDF"
 $txtvalue = "Just a doc"
 Add-Content -Path "C:\storagefiles\demopdf.pdf" -Value $pdfvalue
 Add-content -path "c:\storagefiles\demotxt.txt" -value $txtvalue
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
